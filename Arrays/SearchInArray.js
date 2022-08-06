73c36eedb3623783313e38d7e2ca4d91a5ee2f8d
yash-2507 <imyash.654@gmail.com>
2022-08-07 02:43:53 +0530

    small changes

--- Arrays/SearchInArray.js
+++ Arrays/SearchInArray.js
@@ -6,29 +6,30 @@
  * @param {number} target
  * @return {number}
  */
+
 var search = function (nums, target) {
-    var len = nums.length;
-    var left = 0;
-    var right = len - 1;
-    var mid = 0;
+   var len = nums.length;
+   var left = 0;
+   var right = len - 1;
+   var mid = 0;
 
-    while (left <= right) {
-        mid = left + Math.floor((right - left) / 2);
-        if (nums[mid] === target) return mid;
-        if (nums[mid] > nums[right]) {
-            if (nums[left] <= target && target < nums[mid]) {
-                right = mid - 1;
-            } else {
-                left = mid + 1;
-            }
-        } else {
-            if (nums[mid] < target && target <= nums[right]) {
-                left = mid + 1;
-            } else {
-                right = mid - 1;
-            }
-        }
-    }
+   while (left <= right) {
+      mid = left + Math.floor((right - left) / 2);
+      if (nums[mid] === target) return mid;
+      if (nums[mid] > nums[right]) {
+         if (nums[left] <= target && target < nums[mid]) {
+            right = mid - 1;
+         } else {
+            left = mid + 1;
+         }
+      } else {
+         if (nums[mid] < target && target <= nums[right]) {
+            left = mid + 1;
+         } else {
+            right = mid - 1;
+         }
+      }
+   }
 
-    return -1;
+   return -1;
 };
