05d9877b8d586eb3579fe7fb8705523149c43549
yash-2507 <imyash.654@gmail.com>
2022-08-16 11:06:21 +0530

    Small Change

--- Practice/test.js
+++ Practice/test.js
@@ -1,9 +1,24 @@
-const fibb = (n, memo = {}) => {
-    if(n in memo) {
-        return memo[n]
+const arr = [
+    [1, 1, 0],
+    [1, 0, 1],
+    [0, 0, 0],
+];
+const flipAndInvert = (arr = []) => {
+    const invert = (n) => (n === 1 ? 0 : 1);
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = 0; j < arr[i].length / 2; j++) {
+            const index2 = arr[i].length - 1 - j;
+            if (j === index2) {
+                arr[i][j] = invert(arr[i][j]);
+            } else {
+                const temp = arr[i][j];
+                arr[i][j] = arr[i][index2];
+                arr[i][index2] = temp;
+                arr[i][j] = invert(arr[i][j]);
+                arr[i][index2] = invert(arr[i][index2]);
+            }
+        }
     }
-    if(n === 1 || n === 2) return 1
-    memo[n] =  fibb(n-1) + fibb(n-2);
-    return memo[n]
-}
-console.log(fibb(30));
+};
+flipAndInvert(arr);
+console.log(arr);
